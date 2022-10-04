8e4ca39f18cd601bac01bc04ba6cc931a13c916f
Milagro Scardigno <mdscardigno@gmail.com>
2022-10-03 20:51:31 -0400

    Linq practice

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linq");
+            var scores = new List<int> { 42, 100, 98, 15 };
+            Console.WriteLine(string.Join(", ", scores));
+            //Here is our handy multiply by two function
+            Func<int, int> MultiplyByTwo = value => value * 2;
+
+            //Make a new list to store the results 
+            var newScores = new List<int>();
+
+            //Go through each score in the scores list
+            foreach (var score in scores)
+            {
+                //use the 'MultiplyByTwo' function expression to take the score and double it
+                var doubled = MultiplyByTwo(score);
+                //add it to the new list 
+                newScores.Add(doubled);
+            }
+
+        }
     }
-  }
 }
