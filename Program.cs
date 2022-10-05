d80e254a42bf6a54830949fb64b9c5e11a7137b3
Milagro Scardigno <mdscardigno@gmail.com>
2022-10-05 12:30:34 -0400

    MultiplyByTwo method to user debugger

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ namespace Linq
 {
     class Program
     {
+        static int MultiplyByTwo(int value)
+        {
+            return value * 2;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linq");
+
             var scores = new List<int> { 42, 100, 98, 15 };
             Console.WriteLine(string.Join(", ", scores));
+
             //Here is our handy multiply by two function
-            Func<int, int> MultiplyByTwo = value => value * 2;
+            // Func<int, int> MultiplyByTwo = value => value * 2;
 
             //Make a new list to store the results 
             var newScores = new List<int>();
@@ -25,7 +31,14 @@ namespace Linq
                 //add it to the new list 
                 newScores.Add(doubled);
             }
-
+            //print the list of newScore to the console
+            Console.WriteLine(string.Join(", ", newScores));
+            //Another example of using a function expression
+            // Func<Employee, string, bool> EmployeeHasName = (employee, name) == EmployeeHasName.Name == name;
+            // if (EmployeeHasName(employee, "Bob"))
+            // {
+            //     Console.WriteLine("Yup, that is Bob!");
+            // }
         }
     }
 }
